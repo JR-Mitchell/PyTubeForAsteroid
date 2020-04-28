b0b7bbd1289b5ac2e382caf71cd9a5170ea73b57
JrTheDragon01 <www.aereregion.webs.com@live.co.uk>
2020-04-28 17:44:00 +0100

    Modified tabs.js to give more user feedback

--- tab.js
+++ tab.js
@@ -1,5 +1,7 @@
-function requestUrl(inputObj) {
+function requestUrl(inputObj,buttonObj) {
     return function(){
+        buttonObj.parentNode.removeChild(buttonObj);
+        inputObj.parentNode.removeChild(inputObj);
         var requestData = new FormData();
         requestData.set("url",inputObj.value);
         var request = new XMLHttpRequest();
@@ -7,22 +9,44 @@ function requestUrl(inputObj) {
         request.onload = function(){
             if (request.status == "200") {
                 BODY_CONTENT.appendBreak();
-                BODY_CONTENT.appendText("Successful request!");
+                BODY_CONTENT.appendText("Successful request! Song added with info:");
+                var data = JSON.parse(request.response);
+                BODY_CONTENT.appendBreak();
+                BODY_CONTENT.appendText("Song name: ");
+                BODY_CONTENT.appendText(data["name"]);
+                BODY_CONTENT.appendBreak();
+                BODY_CONTENT.appendText("Artist: ");
+                BODY_CONTENT.appendText(data["artist"]);
+                BODY_CONTENT.appendBreak();
+                BODY_CONTENT.appendText("Duration: ");
+                BODY_CONTENT.appendText(data["duration"]);
             } else {
                 BODY_CONTENT.appendBreak();
-                BODY_CONTENT.appendText("Error code: ");
+                BODY_CONTENT.appendText("Raised error with error code ");
                 BODY_CONTENT.appendText(request.status);
+                BODY_CONTENT.appendText(". Please try again below:");
+                BODY_CONTENT.appendBreak();
+                BODY_CONTENT.appendNode(inputObj);
+                BODY_CONTENT.appendNode(buttonObj);
             }
         };
         request.onerror = function(){
             BODY_CONTENT.appendBreak();
-            BODY_CONTENT.appendText("Request raised unexpected error...");
+            BODY_CONTENT.appendText("Request raised unexpected error. Please try again below:");
+            BODY_CONTENT.appendNode(inputObj);
+            BODY_CONTENT.appendNode(buttonObj);
         };
         request.ontimeout = function(){
             BODY_CONTENT.appendBreak();
-            BODY_CONTENT.appendText("Request timed out...");
+            BODY_CONTENT.appendText("Request timed out. Please try again below:");
+            BODY_CONTENT.appendBreak();
+            BODY_CONTENT.appendNode(inputObj);
+            BODY_CONTENT.appendNode(buttonObj);
         };
         request.send(requestData);
+        BODY_CONTENT.appendText("Sent off request for url ")
+        BODY_CONTENT.appendText(inputObj.value);
+        BODY_CONTENT.appendText(". Awaiting response...")
     }
 }
 
@@ -32,5 +56,5 @@ BODY_CONTENT.appendBreak();
 BODY_CONTENT.appendNode(urlInput);
 var sendButton = document.createElement("button");
 sendButton.innerText = "Request!";
-sendButton.onclick = requestUrl(urlInput);
+sendButton.onclick = requestUrl(urlInput,sendButton);
 BODY_CONTENT.appendNode(sendButton);
